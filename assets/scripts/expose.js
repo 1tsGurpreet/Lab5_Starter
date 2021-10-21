a7350a7a179c9c184da76273372c8774a0dcf567
Gurpreet Singh <g6singh@ucsd.edu>
2021-10-20 18:14:32 -0700

    Finished Expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,79 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const jsConfetti = new JSConfetti();
+
 function init() {
-  // TODO
+  function changePicture(){
+      console.log('this working');
+  }
+
+ // var airHorn = new Image();
+  //airHorn.src = 'C:\\Users\\thegu\\Documents\\HW\\110\\Lab5\\Lab5_Starter\\assets\\images\\air-horn.svg';
+  
+
+  let selectElement = document.getElementById("horn-select");
+  selectElement.addEventListener('change', (event) =>{
+    let horn = event.target.value;
+    let img = document.querySelector("img");
+    let sound = document.querySelector('.hidden');
+    switch(horn){
+      case 'air-horn':
+        img.src = "assets\\images\\air-horn.svg";
+        sound.src = "assets\\audio\\air-horn.mp3"
+        break;
+      case 'car-horn':
+        img.src = "assets\\images\\car-horn.svg";
+        sound.src = "assets\\audio\\car-horn.mp3"
+        break;
+      case 'party-horn':
+        img.src = "assets\\images\\party-horn.svg";
+        sound.src = "assets\\audio\\party-horn.mp3"
+        break;
+      default:
+        console.log("ERROR");
+    }
+  });
+
+  let button = document.querySelector('button');
+  button.addEventListener('click', event => {
+    let sound = document.querySelector('audio');
+    let img = document.querySelector("img");
+
+    if(selectElement.value == 'party-horn'){
+    jsConfetti.addConfetti({
+      emojis: ['🦄'],
+      emojiSize: 100,
+      confettiNumber: 30,
+    })
+    }
+    
+    if(img.src == "assets\\images\\party-horn.svg") console.log("this should work");
+    sound.play();
+    
+  })
+
+  let volume = document.querySelector('input');
+  volume.addEventListener('input', event =>{
+    let img = document.querySelectorAll("img")[1];
+    let sound = document.querySelector('.hidden');
+
+    sound.volume = volume.value/100;
+    if(volume.value == 0){
+      img.src = 'assets\\icons\\volume-level-0.svg'
+    }
+    else if(volume.value < 33){
+      img.src = 'assets\\icons\\volume-level-1.svg'
+    }
+    else if(volume.value < 67){
+      img.src = 'assets\\icons\\volume-level-2.svg'
+      
+    }
+    else {
+      img.src = 'assets\\icons\\volume-level-3.svg'
+    }
+  });
+  
+  
+  
 }
